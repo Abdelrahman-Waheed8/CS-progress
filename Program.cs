b3e8e35e25ab519cecafbbb2f117ce186c6dc1a3
Abdelrahman-Waheed8 <abdothenoobest@gmail.com>
2026-06-11 03:22:50 +0300

    Implemented the Lists declaring, adding, accessing processes

--- Program.cs
+++ Program.cs
@@ -5,7 +5,33 @@ public class CsCollections
 {
     public static void Main(string[] args)
     {
-        // Lists are generic and dynamic arrays 
-        Console.WriteLine("Hi from here");
+        // Lists are generic and dynamic arrays unlike arrays they do not need the size to be specified during initializing
+        // They are also indexed so you can access each element
+
+        // Declaring
+        List<string> names = new List<string>();
+
+        // Adding to the list
+        names.Add("Abdo");
+        names.Add("Omar");
+        names.Add("Joe");
+
+        // Accessing Elements in the list we use two ways
+        //1. Foreach loop
+        //2. For loop
+        Console.WriteLine("Using foreach loop: ");
+        foreach(string name in names)
+        {
+            Console.WriteLine(name);
+        }
+
+        Console.WriteLine("Using for loop: ");
+        // Use the Count attribute that counts the number of items in the list
+        for (int i = 0; i < names.Count; i++)
+        {
+            Console.WriteLine(names[i]);
+        }
+
+
     }
 }

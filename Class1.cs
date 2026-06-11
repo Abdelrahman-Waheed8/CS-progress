2859a3c5f16282b5866eb51fd7a25afbf9439c82
Abdelrahman-Waheed8 <abdothenoobest@gmail.com>
2026-06-11 06:05:53 +0300

    added lists and dictionaries to there own files with attributes called in main program

---
+++ Class1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Dict
+{
+	public Dict()
+	{
+	}
+
+	public void DictionaryUse()
+	{
+		// Now Dictionaries are a new thing which basically store a key and a value (pairs) each key maps to one value. (Key must be Unique!)
+		// Look up in dictionaries basically have time complexity of O(1) since it looks for the value by specific key since it's unique 
+		// Unlike lists which takes O(n)
+
+		// Declaring empty dictionary
+		Dictionary<string, int> scores = new Dictionary<string, int>();
+
+		//// Declaring dictionary with initial values 
+		//Dictionary<string, int> scores = new Dictionary<string, int>
+		//{
+		//	{"Abdo", 19 },
+		//	{"Booody", 29}
+		//};
+
+		//// Another way of Declaring dictionary with initial values
+		//Dictionary<string, int> scores = new Dictionary<string, int>
+		//{
+		//	["Abdo"] = 0,
+		//	["Jojo"] = 1
+		//};
+
+		// Adding items in dictionary Add() accepts pair of data (key, value)
+		scores.Add("Abdo", 50);
+        scores.Add("Jane", 60);
+        scores.Add("Zane", 70);
+		// scores.Add("Abdo", 100); // This Throws an Exception as key "Abdo" already exists
+
+		// Accessing Values using indexer []
+		// Basically you type the key in the indexer and it return the value
+		int AbdoScore = scores["Abdo"]; // 50
+		Console.WriteLine("Abdo score: " + AbdoScore);
+		//int kikoscore = scores["kiko"]; // Throws an Exception also as it's not found
+
+		//Indexers also can add to the dictionary and updates if existing key is passed
+		scores["Omar"] = 80;
+		scores["Zane"] = 95;
+
+		// Checking for items in Dictionary
+		// Like the lists there is ContainsKey() function that accept the key and returns a boolean value whether it's found or not 
+		bool hasAhmed = scores.ContainsKey("Ahmed"); // hasAhmed = false
+
+
+		// TryGetValue() returns true if value found and false if not
+		if(scores.TryGetValue("Abdo", out int Scr))
+		{
+			Console.WriteLine($"Abdo score is {Scr}"); // 50
+		}
+
+		if(scores.TryGetValue("Jamal", out int Scr2)) // key not found then value is 0 (default)
+		{
+			Console.WriteLine($"Jamal score is {Scr2}"); // This won't execute as the condition is false
+		}
+
+		// Looping through dictionaries we use for loops
+		foreach(var pair in scores)
+		{
+			Console.WriteLine($"{pair.Key} score is {pair.Value}");
+		}
+
+		// Another way
+
+		foreach(var (key,value) in scores)
+		{
+            Console.WriteLine($"{key} score is {value}");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,73 +1,15 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 
 public class CsCollections
 {
     public static void Main(string[] args)
     {
-        // Lists are generic and dynamic arrays unlike arrays they do not need the size to be specified during initializing
-        // They are also indexed so you can access each element
+        Lists list = new Lists();
+        list.ListUse();
 
-        // Declaring
-        List<string> names = new List<string>();
-
-        // Adding to the list
-        names.Add("Abdo");
-        names.Add("Omar");
-        names.Add("Joe");
-
-        // Accessing Elements in the list we use two ways
-        //1. Foreach loop
-        //2. For loop
-        Console.WriteLine("Using foreach loop: ");
-        foreach(string name in names)
-        {
-            Console.WriteLine(name);
-        }
-
-        Console.WriteLine("Using for loop: ");
-        // Use the Count attribute that counts the number of items in the list
-        for (int i = 0; i < names.Count; i++)
-        {
-            Console.WriteLine(names[i]);
-        }
-
-        // The last element 
-        Console.WriteLine("Last element: " + names[names.Count - 1]);
-
-        //// Removing 
-        //// In Removing We have two ways: Remove(), RemoveAt()
-        //// Remove() needs the element you want to remove
-        //// RemoveAt() needs the index of element you want to remove
-
-        //names.Remove("Omar"); // now the list is ["Abdo", "Joe"]
-        //names.RemoveAt(0); // now the list is ["Joe"]
-
-        //foreach (string name in names)
-        //{
-        //    Console.WriteLine(name);
-        //}
-
-        // Checking lists
-        // We have Contains(), IndexOf()
-        // Contains() returns boolean value whether the list contains that item or not, expects you to pass the element you want to check that if it's in the list
-        // IndexOf() returns the index of a specified element, expects you pass the element you want to check the index for it
-
-        if(names.Contains("Ahmed")) // the value of the condition is false 
-        {
-            Console.WriteLine("Hello ahmed!");
-        }
-        else
-        {
-            Console.WriteLine("There is no ahmed in list");
-        }
-
-        Console.WriteLine("Index of Joe is: " + names.IndexOf("Joe"));
-        Console.WriteLine("Index of Alan is: " + names.IndexOf("Alan")); // -1 means not found
-
-        // Built-in functions for sorting lists
-        // Sort(), Reverse()
-        names.Sort(); // Sorts the list alphapitcally a-z
-        names.Reverse(); // Reverse sort the list from z-a
+        Dict dict = new Dict();
+        dict.DictionaryUse();
     }
 }

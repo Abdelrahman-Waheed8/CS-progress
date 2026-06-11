d270c5a092becf3a2f82c9838c2fa82b9afdaa4d
Abdelrahman-Waheed8 <abdothenoobest@gmail.com>
2026-06-11 03:13:40 +0300

    Added the program.cs which is the main file i am coding in

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class CsCollections
+{
+    public static void Main(string[] args)
+    {
+        // Lists are generic and dynamic arrays 
+        Console.WriteLine("Hi from here");
+    }
+}

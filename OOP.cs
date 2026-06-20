9e4da32d9db494d4017cda82584b93d6d3738f23
Abdelrahman-Waheed8 <abdothenoobest@gmail.com>
2026-06-20 16:25:33 +0300

    Added Backing fields and auto-properties

--- OOP.cs
+++ OOP.cs
@@ -6,8 +6,33 @@ using System.Threading.Tasks;
 
 public class OOP
 {
-    public OOP() { }
+    /*
+     // Read-write property
+public string Name { get; set; }
 
+// Read-only from outside, settable inside
+public int Age { get; private set; }
+
+// Read-only (must be set in constructor)
+public string Id { get; }
+     
+     */
+
+    // Backing fields
+    private string _name;
+
+    // Auto-Property
+    private string _status { get; set; }
+
+    public OOP(string name) {
+        _name = name;
+    }
+    // Backing fields
+    public string Name
+    {
+        get { return _name; }
+        set { _name = value; }
+    }
     public void Run()
     {
 

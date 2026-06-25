2a2e43c653ba2afea40f6b3d1a35497921723cb6
Abdelrahman-Waheed8 <abdothenoobest@gmail.com>
2026-06-26 01:01:31 +0300

    Added OOP2 file mainly focused on Inheritance concepts which i found new in C# the base keyword

--- OOP2.cs
+++ OOP2.cs
@@ -1,8 +1,31 @@
 using System;
 
-public class objectOriented
+// This is mainly focused on Inheritance
+public class Animal
 {
-    public objectOriented() {}
+    public string Species {get; set;}
+    public Animal(string species) {Species = species;}
 
-    
+// This 'virtual' keyword is actually as an interface for the function and you can override it from the child classes
+    public virtual string makeSound()
+    {
+        return "Sound: ";
+    }
+}
+
+public class Dog : Animal
+{
+    public int legs {get; set;}
+
+// base keyword is for passing arguments to the parent constructor so species is passed to the Animal constructor
+    public Dog(string species, int legs) : base(species) 
+    {
+        this.legs = legs;
+    }
+
+// 'override' overriding the code in the parent class obviously it has to be the same name
+    public override string makeSound()
+    {
+        return base.makeSound() + "Hoof!";
+    }
 }

546d444c3bdaa5ee248e26cf8e539ceadb0810ad
Abdelrahman-Waheed8 <abdothenoobest@gmail.com>
2026-06-29 22:20:02 +0300

    added structs folder to know how to deal with it

---
+++ structsCS.cs
@@ -0,0 +1,22 @@
+using System;
+
+// Define your Point struct here
+public struct Point
+{
+    // Add X and Y fields
+    public int x;
+    public int y;
+}
+
+public class Solution
+{
+    public static string DescribePoint(int x, int y)
+    {
+        // Create a Point with the given x and y values
+        // Return a string in format: "Point(X, Y)"
+        Point p1;
+        p1.x = x;
+        p1.y = y;
+        return $"Point({p1.x}, {p1.y})";
+    }
+}
